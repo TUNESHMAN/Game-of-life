98cbbeafaa74a3e628df6f46fb9b17d725e47fb6
Babatunde <babatundea15@gmail.com>
2020-06-24 09:43:13 +0100

    initiialized the size of the rows and cols and passed it to the grid component as props

--- game/src/components/Grid.js
+++ game/src/components/Grid.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Grid extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className="grid">
+        <h1>Grid</h1>
+      </div>
+    );
+  }
 }
 
-export default Grid
+export default Grid;
--- game/src/components/Main.js
+++ game/src/components/Main.js
@@ -1,19 +1,32 @@
 import React, { Component } from "react";
-import Grid from "./Grid"
+import Grid from "./Grid";
 
 export class Main extends Component {
   constructor() {
     super();
+    // These are not in the state because I will have to make reference to them in the state
+    this.speed = 100; //How fats the game will run
+    this.rows = 30; //The size of the rows
+    this.cols = 50; //The size of the columns
+
     this.state = {
       generation: 0,
+      // This is what the full grid will be like. It is a 2-dimensional, 50 by 30 grid with every element set to false or turned off
+      gridFull: Array(this.rows)
+        .fill()
+        .map(() => Array(this.cols).fill(false)),
     };
   }
   render() {
     return (
       <div>
         <h1>The Game of Life</h1>
-        {/* I created a grid */}
-        <Grid />
+        {/* I created a grid and passed down the state variables as props */}
+        <Grid
+          rows={this.rows}
+          cols={this.cols}
+          gridFull={this.state.gridFull}
+        />
         <h2>Generations: {this.state.generation}</h2>
       </div>
     );
